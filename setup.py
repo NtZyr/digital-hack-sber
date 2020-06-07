d44508af720a6120d4375b68bb092333047bb0b1
Vladislav Surikov <vladislavsurikov@gmail.com>
2020-06-07 09:27:06 +0300

    fix: setup & address class

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lod_dedovschina_sber',
-    version='1.0.1',
+    version='1.0.2',
     author="Дедовщина",
     description="Исходный код задания для кейса от Сбербанка",
     packages=find_packages(),
