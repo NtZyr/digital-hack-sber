0b163ea048562c34b0f7f55bc87f1b221e5a3464
Vladislav Surikov <vladislavsurikov@gmail.com>
2020-06-07 08:47:46 +0300

    fix: version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lod_dedovschina_sber',
-    version='1.0.0',
+    version='1.0.1',
     author="Дедовщина",
     description="Исходный код задания для кейса от Сбербанка",
     packages=find_packages(),
